c0c40a40d29bef777641edec9617a332147f3e37
mg123654 <katteiger@gmail.com>
2025-12-24 23:10:13 +0800

    添加了两种基础的报文解析函数，publish未完善

--- src/mqtt.c
+++ src/mqtt.c
@@ -26,6 +26,101 @@ static unsigned char *pack_mqtt_publish(const mqtt_packet *);
 //
 static const int MAX_LEN_BYTES = 4;
 
+//
+int mqtt_encode_length(unsigned char *buf , size_t len ){
+    int bytes = 0;
+    do{
+        if (bytes+1 > MAX_LEN_BYTES)
+            return bytes;
+        short d =len %0x80;
+        len/=128;
+        //如果还有更多的数据要编码，则将最高位设置为1
+        if (len >0)
+            d |=0x80;
+        buf[bytes++] = (unsigned char)d;
+    
+        } while (len >0);
+        return bytes;
+  
+}
+
+
+unsigned long long mqtt_decode_length(const unsigned char **buf){
+    int multiplier =1;
+    long long value =0;
+    unsigned char encodedByte;
+    int bytes =0;
+    do {
+        encodedByte = unpack_u8(buf);
+        value += (encodedByte &0x7F) * multiplier;
+        multiplier *=128;
+        bytes++;
+
+    } while ((encodedByte &0x80) !=0);
+    return value;
+}
+
+
+//传入的buf指针指向起始位置后面第一个字节（猜测是固定抱头的第二个字节）
+static size_t unpack_mqtt_connect(const unsigned char *buf, mqtt_header *hdr,mqtt_packet* pkt){
+    mqtt_connect connect={.header=*hdr};
+    //结构体值拷贝操作，直接覆盖旧的结构体内存
+    pkt->connect=connect;
+    const unsigned char *init = buf;
+
+    size_t len = mqtt_decode_length(&buf);
+    
+    //init指针始终保持原位，buf用来移动。
+    buf = init + 8;
+
+    pkt->connect.byte=unpack_u8((const uint8_t**)&buf);
+    pkt->connect.payload.keepalive=unpack_u16((const uint16_t**)&buf);
+
+    //获取客户端的可变长ID，长度由前面两个字节表示
+    unpack_string16((uint8_t**)&buf,pkt->connect.payload.client_id);
+    
+    //根据标志位获取遗嘱主题和遗嘱消息
+    if (pkt->connect.bits.will){
+        unpack_string16((uint8_t**)&buf,pkt->connect.payload.will_topic);
+        unpack_string16((uint8_t**)&buf,pkt->connect.payload.will_message);
+    }
+
+    //根据标志位获取用户名和密码
+    if (pkt->connect.bits.username){
+        unpack_string16((uint8_t**)&buf,pkt->connect.payload.username);
+    }
+
+    if (pkt->connect.bits.password){
+        unpack_string16((uint8_t**)&buf,pkt->connect.payload.password);
+    }
+    return len;
+}
+
+
+static size_t unpack_mqtt_publish(const unsigned char *buf ,mqtt_header *hdr,mqtt_packet* pkt){
+    mqtt_publish publish ={.header = *hdr};
+    pkt->publish=publish;
+
+    size_t len=mqtt_decode_length((const unsigned char**)&buf);
+
+    pkt->publish.topic_len=unpack_string16((const uint16_t**)&buf,(uint8_t*)pkt->publish.topic);
+
+    size_t message_len=len;
+
+    //pkt_id只会在QoS大于0时候出现，用于进行消息确认
+    if(pkt->publish.header.bits.qos>AT_MOST_ONCE){
+        pkt->publish.pkt.id=
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
